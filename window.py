d40a70df8e545d178f7e3ce720b73fa578d19e46
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2023-02-19 11:34:27 -0300

    Apolicando a função Creat_Qr e criando os TextFields

--- window.py
+++ window.py
@@ -1,8 +1,20 @@
 from kivymd.app import MDApp
 from kivymd.app import Builder
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.button import MDRectangleFlatButton
+from Generator_Qr import Creat_Qr
 
-class App(MDApp):
+class MainFrame(MDApp):
     def build(self):
         self.theme_cls.theme_style = 'Dark'
         self.__FILE = Builder.load_file('properties.kv')
+        self.__ID = self.__FILE.ids
         return self.__FILE
+
+    def generate(self):
+        Creat_Qr(self.__ID.rewrite_text.text,0)
+
+if __name__ == '__main__':
+    MainFrame().run()
