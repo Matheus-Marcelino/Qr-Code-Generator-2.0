f205a0b2a7aa9cb9783f56958c4fc0bfbaf68358
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2023-02-24 13:51:58 -0300

    finalizado

--- Generator_Qr.py
+++ Generator_Qr.py
@@ -10,27 +10,27 @@ def Creat_Qr(text: str, view: bool) -> str:
 
     Args:
         text (str): Texto que será inserido do Qr Code
-        view (int): 1 -> mostra a imagem na tela
-                    0 -> Não mostra a imagem na tela
+        view (bool): True -> mostra a imagem na tela
+                     False -> Não mostra a imagem na tela
 
     Returns:
         str: Caminho do arquivo
     """
+    if '.png' not in text:
+        text = text + '.png'
+
     treated_text = text.replace('/', ' ')
-    treated_text = treated_text.replace('\\', ' ') + '.png'
+    treated_text = treated_text.replace('\\', ' ')
     
     qr = QRCode()
     qr.add_data(text)
-
-    if '.png' not in text:
-        text = text + '.png'
 
     FILE: str = dirname(realpath(__file__)) + '\output\\'
 
     qr_img = qr.make_image()
     try:
         qr_img.save(f'{FILE}{treated_text}')
-        if view == 1:
+        if view:
             webopen(FILE+treated_text)  # mostra o Qr code na tela
         return FILE+treated_text
     except FileNotFoundError:
